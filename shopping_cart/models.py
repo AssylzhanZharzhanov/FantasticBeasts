bc1debfabcc1cb3edbeff3160fb10dfed8c59686
Assylzhan Zharzhanov <asik.zharzhanov@gmail.com>
2018-12-19 15:27:52 +0600

    'files added'

--- shopping_cart/models.py
+++ shopping_cart/models.py
@@ -7,9 +7,11 @@ class Cart(models.Model):
     owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     items = models.ManyToManyField(Beast)
     date_created = models.DateTimeField(auto_now=True)
+    amount = models.IntegerField(default=1)
+    #
+    # def __str__(self):
+    #     return self.o
 
-    def __str__(self):
-        return self.items
 
 def create_cart(request):
     user = User.objects.get(username=request.user)
--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, reverse
 from .models import Cart
 from .models import Beast
 from django.contrib.auth.models import User
 
 
 def add_to_cart(request,id):
-    item = Beast.objects.get(id = id)
-    cart = Cart.objects.get(owner__username=request.user)
-    cart.items.add(item)
+    item = Beast.objects.get(id=id)
+    user_cart = Cart.objects.get(owner__username=request.user)
+    user_cart.items.add(item)
+    user_cart.save()
+
+    if item.type == 'Animal':
+        return redirect('animals')
+    elif item.type == 'Bird':
+        return redirect('birds')
+    else:
+        return redirect('insects')
+
+def delete_from_cart(request, id):
+    user_cart = Cart.objects.get(owner__username=request.user)
+    item = Beast.objects.get(id=id)
+    user_cart.items.remove(item)
+    user_cart.save()
+    return cart(request)
 
 
 def cart(request):
     user_cart = Cart.objects.get(owner__username=request.user)
     items = user_cart.items.all()
-    # amount_items = items.count() 'amount': amount_items
+    amount_items = items.count()
+    # 'amount': amount_items
     return render(request, 'zoo/cart.html', {'items': items,})
+
+
+def refresh(request, id):
+    amount = request.POST.get('number')
+
+
